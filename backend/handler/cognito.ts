b680a7b984d01db459c75871379058d24b6d6e4b
= <ChanakanThongrong@gmail.com>
2025-05-16 14:38:14 +0700

    feat(auth): add cognitoMiddleware

--- backend/handler/cognito.ts
+++ backend/handler/cognito.ts
@@ -1,22 +1,26 @@
-import { Hono } from 'hono';
-import { addUserToGroup } from '../database/service/cognito.ts'; // ปรับเส้นทางให้ถูกต้อง
+import { Hono } from "hono";
+import { addUserToGroup } from "../database/service/cognito.ts"; // ปรับเส้นทางให้ถูกต้อง
+import { cognitoMiddleware } from "../middleware.ts";
 export const cognitoApp = new Hono();
 
-cognitoApp.get('/', (c) => {
-  return c.json({ message: 'GET /cognito' });
+cognitoApp.get("/", (c) => {
+  return c.json({ message: "GET /cognito" });
 });
 
-cognitoApp.post('/add-to-group', async (c) => {
+cognitoApp.use(cognitoMiddleware);
+
+cognitoApp.post("/add-to-group", async (c) => {
   const { username, groupName } = await c.req.json();
 
   if (!username || !groupName) {
-    return c.json({ error: 'Missing username or groupName' }, 400);
+    return c.json({ error: "Missing username or groupName" }, 400);
   }
 
   try {
     await addUserToGroup(username, groupName);
     return c.json({ success: true });
   } catch (_err) {
-    return c.json({ error: 'Failed to add user to group' }, 500);
+    return c.json({ error: "Failed to add user to group" }, 500);
   }
 });
+
