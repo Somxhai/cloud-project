3a8ba910221bc3d93f8f82e022a2622df41e1bd8
ThanapoomPol <thanapoom.pol@dome.tu.ac.th>
2025-05-23 15:02:17 +0700

    fix bug

--- frontend/src/app/(public)/auth/student-signup/page.tsx
+++ frontend/src/app/(public)/auth/student-signup/page.tsx
@@ -203,7 +203,7 @@ export default function StudentSignUpPage() {
 				formData.append("file", profilePictureFile);
 
 				const res = await fetch(
-					"http://localhost:8000/upload/upload-image",
+					`${process.env.NEXT_PUBLIC_API_BASE_URL}/upload/upload-image`,
 					{
 						method: "POST",
 						headers: {
